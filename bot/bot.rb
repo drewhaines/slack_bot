c548b6ed94f8b6fb9452cb448becff8b89191788
Drew Haines <drewhaines3@gmail.com>
2017-07-12 09:52:53 -0700

    add help section, add pong and gif me commands

--- bot/bot.rb
+++ bot/bot.rb
@@ -5,6 +5,36 @@ class Bot < SlackRubyBot::Bot
     @id = @id % 10 + 1
   end
 
+  help do
+    title 'Fairway Bot'
+    desc 'This bot does various things that make life at Fairway easier. =)'
+
+    command 'ping' do
+      desc 'Responds with "pong" to let you know Fairway Bot is running.'
+    end
+
+    command 'hi' do
+      desc 'Fairway Bot politely says hello.'
+    end
+
+    command 'say <expression>' do
+      desc 'Fairway Bot repeats your expression and logs it at https://fairbot.herokuapp.com.'
+    end
+
+    command 'time' do
+      desc 'Fairway Bot tells you the time (Pacific Timezone).'
+    end
+
+    command 'gif me <expression>' do
+      desc 'Fairway Bot responds with a gif for your expression.'
+    end
+  end
+
+
+  command 'ping' do |client, data, match|
+    client.say(text: 'pong', channel: data.channel)
+  end
+
   command 'say' do |client, data, match|
     Rails.cache.write next_id, { text: match['expression'] }
     client.say(channel: data.channel, text: match['expression'])
@@ -12,5 +42,9 @@ class Bot < SlackRubyBot::Bot
 
   command 'time' do |client, data, match|
     client.say(channel: data.channel, text: Time.now.to_s)
+  end
+
+  command 'gif me' do |client, data, match|
+    client.say(channel: data.channel, text: "Here is your gif for " + match['expression'].to_s, gif: match['expression'])
   end
 end
